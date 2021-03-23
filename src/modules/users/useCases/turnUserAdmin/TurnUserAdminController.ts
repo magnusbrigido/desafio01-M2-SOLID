8ab99eb853bf5f9d2748fbe86307e8fec7f5bfb5
Magnus Freire <magnus.brigido@gmail.com>
2021-03-22 21:32:46 -0300

    Implementação do código de acordo com o princípios SOLID

--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
@@ -6,7 +6,15 @@ class TurnUserAdminController {
   constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = <{ user_id: string }>request.params;
+
+    try {
+      const user = this.turnUserAdminUseCase.execute({ user_id });
+
+      return response.send(user);
+    } catch (error) {
+      return response.status(404).json({ error: error.message });
+    }
   }
 }
 
