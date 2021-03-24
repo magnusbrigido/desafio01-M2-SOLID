944fb5d886b3188d233851e7a0048b3c4d78cfe0
Magnus Freire <magnus.brigido@gmail.com>
2021-03-24 19:11:44 -0300

    Adição da documentação com swagger

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import express from "express";
+import SwaggerUi from "swagger-ui-express";
 
 import { usersRoutes } from "./routes/users.routes";
+import swaggerFile from "./swagger.json";
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api-docs", SwaggerUi.serve, SwaggerUi.setup(swaggerFile));
 
 app.use("/users", usersRoutes);
 
